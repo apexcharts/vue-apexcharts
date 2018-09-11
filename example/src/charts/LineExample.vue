6b628664ea647d5f934890d95f3bb7c3867ef478
Juned Chhipa <juned.chhipa@gmail.com>
2018-09-11 13:36:06 +0530

    reproducing typeError on series update

--- example/src/charts/LineExample.vue
+++ example/src/charts/LineExample.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="example">
     <apexcharts width="500" height="350" type="line" :options="chartOptions" :series="series"></apexcharts>
+    <div>
+       <button @click="updateChart">Update!</button>
+    </div>
   </div>
 </template>
 
@@ -17,14 +20,61 @@ export default {
     return {
       chartOptions: {
         xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          type: 'datetime',
+          categories: ['01/01/2003', '02/01/2003','03/01/2003','04/01/2003','05/01/2003','06/01/2003','07/01/2003','08/01/2003'],
         },
       },
       series: [{
         name: 'series-1',
         data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }, {
+        name: 'series-2',
+        data: [23, 43, 54, 12, 44, 52, 32, 11]
       }]
     }
   },
+  methods: {
+      generateDayWiseTimeSeries(baseval, count, yrange) {
+        var i = 0;
+        var series = [];
+        while (i < count) {
+          var x = baseval;
+          var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+          series.push([x, y]);
+          baseval += 86400000;
+          i++;
+        }
+        return series;
+      },
+      updateChart() {
+        let series = [
+            {
+            name: 'South',
+            data: this.generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
+              min: 10,
+              max: 60
+            })
+          },
+          {
+            name: 'North',
+            data: this.generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
+              min: 10,
+              max: 20
+            })
+          },
+          
+          {
+            name: 'Central',
+            data: this.generateDayWiseTimeSeries(new Date('11 Feb 2017').getTime(), 20, {
+              min: 10,
+              max: 15
+            })
+          }
+        ]
+
+        this.series = series
+      }
+  }
 }
 </script>
