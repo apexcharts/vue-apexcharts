a189b841d948b16e8936a66ebfd3973f519fa9eb
Juned Chhipa <juned.chhipa@gmail.com>
2019-02-26 08:46:54 +0530

    replace ApexCharts.merge function

--- src/ApexCharts.component.js
+++ src/ApexCharts.component.js
@@ -78,9 +78,55 @@ export default {
         newOptions.chart.events[evt] = this.$listeners[evt];
       });
 
-      const config = ApexCharts.merge(this.options, newOptions);
+      const config = this.merge(this.options, newOptions);
       this.chart = new ApexCharts(this.$el, config)
       this.chart.render()
+    },
+    merge(target, source) {
+      if (typeof Object.assign !== 'function') {
+        (function() {
+          Object.assign = function(target) {
+            'use strict'
+            // We must check against these specific cases.
+            if (target === undefined || target === null) {
+              throw new TypeError('Cannot convert undefined or null to object')
+            }
+  
+            let output = Object(target)
+            for (let index = 1; index < arguments.length; index++) {
+              let source = arguments[index]
+              if (source !== undefined && source !== null) {
+                for (let nextKey in source) {
+                  if (source.hasOwnProperty(nextKey)) {
+                    output[nextKey] = source[nextKey]
+                  }
+                }
+              }
+            }
+            return output
+          }
+        })()
+      }
+  
+      let output = Object.assign({}, target)
+      if (this.isObject(target) && this.isObject(source)) {
+        Object.keys(source).forEach((key) => {
+          if (this.isObject(source[key])) {
+            if (!(key in target)) {
+              Object.assign(output, {
+                [key]: source[key]
+              })
+            } else {
+              output[key] = this.extend(target[key], source[key])
+            }
+          } else {
+            Object.assign(output, {
+              [key]: source[key]
+            })
+          }
+        })
+      }
+      return output
     },
     refresh () {
       this.destroy()
