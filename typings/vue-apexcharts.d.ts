00b770eedf903b2a50b5cf876bed36d897c6e5df
Juned Chhipa <juned.chhipa@gmail.com>
2020-04-04 15:40:12 +0530

    add polarArea in types

--- typings/vue-apexcharts.d.ts
+++ typings/vue-apexcharts.d.ts
@@ -7,7 +7,7 @@ interface VueApexChartsComponent extends Vue {
   readonly chart?: ApexCharts;
   // props
   options?: ApexOptions;
-  type?: 'line' | 'area' | 'bar' | 'histogram' | 'pie' | 'donut' | 'radialBar' | 'rangeBar' | 'scatter' | 'bubble' | 'heatmap' | 'candlestick' | 'radar';
+  type?: 'line' | 'area' | 'bar' | 'histogram' | 'pie' | 'donut' | 'radialBar' | 'rangeBar' | 'scatter' | 'bubble' | 'heatmap' | 'candlestick' | 'radar' | 'polarArea';
   series: any;
   width?: string | number;
   height?: string | number;
